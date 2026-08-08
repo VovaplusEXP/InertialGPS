e773fa4ac85bd36a04b415655193b730839aaaed
Vova+ <machihinvladimir96@gmail.com>
2026-08-08 14:20:09 +0300

    Fix unresolved ContextCompat reference

--- app/src/main/java/com/example/inertialgps/MockLocationService.kt
+++ app/src/main/java/com/example/inertialgps/MockLocationService.kt
@@ -22,6 +22,7 @@ import android.os.Looper
 import android.os.SystemClock
 import android.util.Log
 import androidx.core.app.NotificationCompat
+import androidx.core.content.ContextCompat
 import kotlin.math.cos
 import kotlin.math.PI
 
